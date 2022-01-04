4684b1d496c1f4f57b349888b9e3949581119213
Gyeongryun <c7yung0@gmail.com>
2022-01-04 16:18:32 +0900

    은진시 발표 좀 해주시궛어요?

---
+++ src/학년별이수자비율A.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./data/1. 19년도-과정이수및평가점수.csv', usecols=[2,4])
+# df.head()
+
+all = df.groupby(['학년']).count()
+# all.head()
+
+comp = df[df['A과목이수'] == 1]
+compc = comp.groupby(['학년']).count()
+# compc.head()
+
+rate=compc/all
+rater=rate.round(2)
+# rater.head()
+
+ratedesc=rater.sort_values('A과목이수',ascending=False)
+ratedesc.head()
