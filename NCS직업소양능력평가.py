443a17206c413a945f6bed7880fed74b0c09d88f
JiHee <kimjihee1113@naver.com>
2022-01-04 11:20:01 +0900

    A과목

--- NCS직업소양능력평가.py
+++ NCS직업소양능력평가.py
@@ -1,12 +1,12 @@
 import pandas as pd
+import numpy as np
 
-total = pd.read_csv ('./data/1519전체집계.csv')
+total = pd.read_csv ('./data/19년도평가.csv',encoding='EUC-KR', index_col='학과') # 제일 첫번째로 오게 만듬
 total.head()
-
-#칼럼정보
-total.columns 
-#Index(['연도', '학과', '학생구분', '응시자수', 'A과목', 'B과목', 'F과목', 'D과목', 'G과목', 'C과목', 'E과목'], dtype='object')
-#인덱스정보  RangeIndex(start=0, stop=278, step=1)
-total.index 
-total.to_numpy()
 total.info()
+total.columns
+#Index(['연도', '학과', '학생구분', '응시자수', 'A과목', 'B과목', 'F과목', 'D과목', 'G과목', 'C과목', 'E과목'], dtype='object')
+
+
+total['A과목이수']
+#Name: A과목이수, Length: 3478, dtype: int64
