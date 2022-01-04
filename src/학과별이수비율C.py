c3c221de84b05608d052e07bce6461b4d6ff84b0
Gyeongryun <c7yung0@gmail.com>
2022-01-04 12:25:36 +0900

    증식

---
+++ src/학과별이수비율C.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./data/1. 19년도-과정이수및평가점수.csv', usecols=[1,6])
+# df.head()
+
+all = df.groupby(['학과']).count()
+# all.head()
+
+comp = df[df['C과목이수'] == 1]
+compc = comp.groupby(['학과']).count()
+# compc.head()
+
+rate=compc/all
+rater=rate.round(2)
+# rater.head()
+
+ratedesc=rater.sort_values('C과목이수',ascending=False)
+ratedesc.head(5)
---
+++ src/학과별이수비율D.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./data/1. 19년도-과정이수및평가점수.csv', usecols=[1,7])
+# df.head()
+
+all = df.groupby(['학과']).count()
+# all.head()
+
+comp = df[df['D과목이수'] == 1]
+compc = comp.groupby(['학과']).count()
+# compc.head()
+
+rate=compc/all
+rater=rate.round(2)
+# rater.head()
+
+ratedesc=rater.sort_values('D과목이수',ascending=False)
+ratedesc.head(5)
