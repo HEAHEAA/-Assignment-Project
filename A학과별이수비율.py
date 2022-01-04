72d1342d38ea690c3757dc4262aa59ad52afe9b0
Gyeongryun <c7yung0@gmail.com>
2022-01-04 11:52:27 +0900

    경륜

---
+++ A학과별이수비율.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./data/1. 19년도-과정이수및평가점수.csv', usecols=[1,4])
+# df.head()
+
+all = df.groupby(['학과']).count()
+# all.head()
+
+comp = df[df['A과목이수'] == 1]
+compc = comp.groupby(['학과']).count()
+# compc.head()
+
+rate=compc/all
+rate.head()
