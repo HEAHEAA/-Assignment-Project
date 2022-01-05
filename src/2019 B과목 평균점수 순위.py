81a9f5ac86dc67e88c6856c03d96ae58687511de
JiHee <kimjihee1113@naver.com>
2022-01-05 15:39:54 +0900

    평균점수 순위입니다

---
+++ src/2019 B과목 평균점수 순위.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./data/2. 15-19년전체집계.csv', usecols=[0,1,5])
+df.head()
+
+is_2019 = df['연도'] == 2019
+year2019 = df[is_2019]
+year2019
+year2019.info()
+
+ratedesc=year2019.sort_values('B과목',ascending=False)
+ratedesc
