da3a91bb1917111d5628a68f53527fed6e7e7a33
Gyeongryun <c7yung0@gmail.com>
2022-01-06 16:01:01 +0900

    커밋해줘~

--- src/4-1. 년도별응시자예측.py
+++ src/4-1. 년도별응시자예측.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_path = "./../csv_file/market-price_2021.csv"
-bitcoin_df = pd.read_csv(file_path, names = ['day', 'price'])
+# file_path = "./../csv_file/market-price_2021.csv"
+# bitcoin_df = pd.read_csv(file_path, names = ['day', 'price'])
 
-bitcoin_train_df = bitcoin_df.iloc[:361]
+# bitcoin_train_df = bitcoin_df.iloc[:361]
--- src/5-1. 년도별응시자추이 copy.py
+++ src/5-1. 년도별응시자추이 copy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('./data/numofdep.csv')
-df.head()
-df.groupby(['연도']).size
+# df = pd.read_csv('./data/numofdep.csv')
+# df.head()
+# df.groupby(['연도']).size
 
